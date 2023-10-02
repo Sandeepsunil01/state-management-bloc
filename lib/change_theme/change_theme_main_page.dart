d1a8ab9a2d4e92d0b927ed989f87f9637d5d618b
sandeep <sandeepsunil01@gmail.com>
2023-10-02 10:22:12 +0530

    Change Theme with Bloc and Cubit

--- lib/change_theme/change_theme_main_page.dart
+++ lib/change_theme/change_theme_main_page.dart
@@ -1,46 +1,61 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:state_management_bloc/change_theme/theme/theme_bloc.dart';
+import 'package:state_management_bloc/change_theme/theme_cubit/theme_cubit.dart';
 
 class ChangeThemeApp extends StatelessWidget {
   const ChangeThemeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<ThemeBloc>(
-      create: (context) => ThemeBloc(),
-      // This is Using Bloc Builder
-      // child: BlocBuilder<ThemeBloc, ThemeState>(
-      //   builder: (context, state) {
-      //     return MaterialApp(
-      //       title: "Change Theme",
-      //       debugShowCheckedModeBanner: false,
-      //       theme: state.appTheme == AppTheme.light
-      //           ? ThemeData.light(
-      //               useMaterial3: true,
-      //             )
-      //           : ThemeData.dark(
-      //               useMaterial3: true,
-      //             ),
-      //       home: const ThemePage(),
-      //     );
-      //   },
-      // ),
+    // return BlocProvider<ThemeBloc>(
+    //   create: (context) => ThemeBloc(),
+    //   // This is Using Bloc Builder
+    //   // child: BlocBuilder<ThemeBloc, ThemeState>(
+    //   //   builder: (context, state) {
+    //   //     return MaterialApp(
+    //   //       title: "Change Theme Bloc",
+    //   //       debugShowCheckedModeBanner: false,
+    //   //       theme: state.appTheme == AppTheme.light
+    //   //           ? ThemeData.light(
+    //   //               useMaterial3: true,
+    //   //             )
+    //   //           : ThemeData.dark(
+    //   //               useMaterial3: true,
+    //   //             ),
+    //   //       home: const ThemePage(),
+    //   //     );
+    //   //   },
+    //   // ),
 
-      // With using extensions method
-      // If Builder is not used then the bloc doenst get the context
-      child: Builder(builder: (context) {
-        return MaterialApp(
-          title: "Change Theme",
-          debugShowCheckedModeBanner: false,
-          theme: context.watch<ThemeBloc>().state.appTheme == AppTheme.light
-              ? ThemeData.light(useMaterial3: true)
-              : ThemeData.dark(useMaterial3: true),
-          home: const ThemePage(),
-        );
-      }),
+    //   // With using extensions method
+    //   // If Builder is not used then the bloc doenst get the context
+    //   // child: Builder(builder: (context) {
+    //   //   return MaterialApp(
+    //   //     title: "Change Theme Bloc",
+    //   //     debugShowCheckedModeBanner: false,
+    //   //     theme: context.watch<ThemeBloc>().state.appTheme == AppTheme.light
+    //   //         ? ThemeData.light(useMaterial3: true)
+    //   //         : ThemeData.dark(useMaterial3: true),
+    //   //     home: const ThemePage(),
+    //   //   );
+    //   // }),
+    // );
+
+    return BlocProvider<ThemeCubit>(
+      create: (context) => ThemeCubit(),
+      child: BlocBuilder<ThemeCubit, ThemeState>(
+        builder: ((context, state) {
+          return MaterialApp(
+            title: "Change Theme Cubit",
+            debugShowCheckedModeBanner: false,
+            theme: state.appTheme == AppTheme.light
+                ? ThemeData.light(useMaterial3: true)
+                : ThemeData.dark(useMaterial3: true),
+            home: const ThemePage(),
+          );
+        }),
+      ),
     );
   }
 }
@@ -59,10 +74,13 @@ class ThemePage extends StatelessWidget {
           onPressed: () {
             final int randomInt = Random().nextInt(10);
             debugPrint("Random Number == $randomInt");
+            // This is Called for Bloc
+            // context
+            //     .read<ThemeBloc>()
+            //     .add(ChangeThemeEvent(randomInt: randomInt));
 
-            context
-                .read<ThemeBloc>()
-                .add(ChangeThemeEvent(randomInt: randomInt));
+            // This is called for Cubit
+            context.read<ThemeCubit>().changeTheme(randomInt);
           },
           child: const Text(
             "Change Theme",
---
+++ lib/change_theme/theme_cubit/theme_cubit.dart
@@ -0,0 +1,16 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'theme_state.dart';
+
+class ThemeCubit extends Cubit<ThemeState> {
+  ThemeCubit() : super(ThemeState.initial());
+
+  void changeTheme(int randomInt) {
+    if (randomInt.isEven) {
+      emit(state.copyWith(appTheme: AppTheme.light));
+    } else {
+      emit(state.copyWith(appTheme: AppTheme.dark));
+    }
+  }
+}
